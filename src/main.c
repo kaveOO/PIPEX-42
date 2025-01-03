18e2057cec24b4738138028ac55db283ca16eb2c
kaveO <alban.billiette@gmail.com>
2025-01-03 01:40:16 +0100

    + | Ajout du src/main avec explication de **envp le main est vide pour le moment

--- src/main.c
+++ src/main.c
@@ -1,18 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   albillie.h                                         :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaveo <kaveo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/02 14:44:46 by kaveo             #+#    #+#             */
-/*   Updated: 2025/01/03 01:01:14 by kaveo            ###   ########.fr       */
+/*   Created: 2025/01/03 01:36:00 by kaveo             #+#    #+#             */
+/*   Updated: 2025/01/03 01:38:52 by kaveo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
 
-#include <unistd.h>
+// ? **envp est un pointeur sur la liste des variables d'environnement pour aller chercher les commandes.
+int	main(int argc, char **argv, char **envp)
+{
 
-#endif
+
+}
